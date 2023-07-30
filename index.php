9f97906c06fcf8ccc6806c110c0761e9eff9da20
Amirali Hosseini <amiralihosseini216@yahoo.com>
2023-07-30 19:20:28 +0330

    proct base done

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
 
-include('system/config.php');
-include(__DIR__ . '/system/router/Routing.php');
-include('system/traits/Redirect.php');
-include('system/traits/View.php');
-include('app/controllers/Controller.php');
-include('app/controllers/Home.php');
 include('system/bootstrap/boot.php');
