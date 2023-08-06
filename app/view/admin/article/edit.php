f4294bfaf34f9d90d1f85f0d80f63695d78e7b10
Amirali Hosseini <amiralihosseini216@yahoo.com>
2023-08-06 11:54:23 +0330

    article view semi done

--- app/view/admin/article/edit.php
+++ app/view/admin/article/edit.php
@@ -1,21 +1,23 @@
 <?php $this->include("admin.layouts.header") ?>
 
-                <form>
-                    <section class="form-group">
-                        <label for="title">Title</label>
-                        <input type="text" class="form-control" id="title" placeholder="title ...">
-                    </section>
-                    <section class="form-group">
-                        <label for="cat_id">Category</label>
-                        <select class="form-control" id="cat_id">
-                            <option value="1">Sport</option>
-                            <option value="2">News</option>
-                        </select>
-                    </section>
-                    <section class="form-group">
-                        <label for="body">Body</label>
-                        <textarea class="form-control" id="body" rows="5" placeholder="body ..."></textarea>
-                    </section>
-                    <button type="submit" class="btn btn-primary">Update</button>
-                </form>
-                <?php $this->include("admin.layouts.footer") ?>
+<form action="<?php $this->url('article/update/' . $article['id']) ?>" method="post">
+    <section class="form-group">
+        <label for="title">Title</label>
+        <input type="text" class="form-control" id="title" name="title" value="<?= $article['title'] ?>">
+    </section>
+    <section class="form-group">
+        <label for="cat_id">Category</label>
+        <select class="form-control" id="cat_id">
+            <?php foreach($categories as $category){ ?>
+            <option value="<?= $category['id'] ?>" <?php if($article['cat_id'] == $category['id']) echo 'selected' ?>><?= $category['name'] ?></option>
+            <?php } ?>
+        </select>
+    </section>
+    <section class="form-group">
+        <label for="body">Body</label>
+        <textarea class="form-control" id="body" rows="5" value="<?= $article['body'] ?>"></textarea>
+    </section>
+    <button type="submit" class="btn btn-primary">Update</button>
+</form>
+
+<?php $this->include("admin.layouts.footer") ?>
